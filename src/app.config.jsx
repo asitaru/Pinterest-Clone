1d63c922afac42990cc807b7186112ba98a8fc3a
asitaru <andreisitaru93@gmail.com>
2018-02-19 23:05:59 +0200

    Fix for the previous commit -set the requests url to production -changed a CDN to https

--- src/app.config.jsx
+++ src/app.config.jsx
@@ -1,6 +1,6 @@
 export default {
-    //baseUrl: 'https://react-pinterest.herokuapp.com/',
-    baseUrl: 'http://localhost:4000/',
+    baseUrl: 'https://react-pinterest.herokuapp.com/',
+    //baseUrl: 'http://localhost:4000/',
     validation: {
         email: "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{1,63}$",
         name: "[A-Za-z] [A-Za-z]"
